0e0cce52ed287c87ba33596fb28c4c38bd05ace5
shivasankari3101 <shivasankari3101@gmail.com>
2021-07-13 12:21:18 +0530

    MotionPath Plugin practice

--- circular/index.js
+++ circular/index.js
@@ -1,48 +1,66 @@
 const t1 = gsap.timeline();
 
-t1.fromTo(".text1",{yPercent:0},{yPercent:-100},"same1")
-  .fromTo(".img1",{yPercent:0},{yPercent:-100},"same1")
-  .addLabel("same1")
-  .fromTo(".text2",{yPercent:100},{yPercent:-100},"same2")
-  .fromTo(".img2",{yPercent:100},{yPercent:-100},"same2")
-  .addLabel("same2")
-  .fromTo(".text3",{yPercent:100},{yPercent:-100},"same3")
-  .fromTo(".img3",{yPercent:100},{yPercent:-100},"same3")
-  .addLabel("same3")
-  .fromTo(".text4",{yPercent:100},{yPercent:-100},"same4")
-  .fromTo(".img4",{yPercent:100},{yPercent:-100},"same4")
-  .addLabel("same4")
-  .from(".slogan",{yPercent:100})
-  .fromTo(".overlay",{xPercent:10},{xPercent:100});
+t1.fromTo(".text1", { yPercent: 0 }, { yPercent: -100 }, "same1")
+    .fromTo(".img1", { yPercent: 0 }, { yPercent: -100 }, "same1")
+    .addLabel("same1")
+    .fromTo(".text2", { yPercent: 100 }, { yPercent: -100 }, "same2")
+    .fromTo(".img2", { yPercent: 100 }, { yPercent: -100 }, "same2")
+    .addLabel("same2")
+    .fromTo(".text3", { yPercent: 100 }, { yPercent: -100 }, "same3")
+    .fromTo(".img3", { yPercent: 100 }, { yPercent: -100 }, "same3")
+    .addLabel("same3")
+    .fromTo(".text4", { yPercent: 100 }, { yPercent: -100 }, "same4")
+    .fromTo(".img4", { yPercent: 100 }, { yPercent: -100 }, "same4")
+    .addLabel("same4")
+    .from(".slogan", { yPercent: 100 })
+    .fromTo(".overlay", { xPercent: 10 }, { xPercent: 100 });
 
 
 
 ScrollTrigger.create({
-    animation:t1,
-    trigger:".section1",
-    scrub:true,
-    pin:true
+    animation: t1,
+    trigger: ".section1",
+    scrub: true,
+    pin: true
 })
 
 
 const t2 = gsap.timeline();
 
-t2.to(".ring_box img",{rotation:45},"same1")
-  .fromTo(".p1", {yPercent:0},{yPercent:-100},"same1")
-  .to(".ring_box img",{rotation:90},"same2")
-  .fromTo(".p2", {yPercent:100},{yPercent:-100},"same2")
-  .to(".ring_box img",{rotation:120},"same3")
-  .fromTo(".p3", {yPercent:100},{yPercent:0},"same3")
-  .addLabel("same1")
-  .addLabel("same2")
-  .addLabel("same3");
+t2.to(".ring_box img", { rotation: 45 }, "same1")
+    .fromTo(".p1", { yPercent: 0 }, { yPercent: -100 }, "same1")
+    .to(".ring_box img", { rotation: 90 }, "same2")
+    .fromTo(".p2", { yPercent: 100 }, { yPercent: -100 }, "same2")
+    .to(".ring_box img", { rotation: 120 }, "same3")
+    .fromTo(".p3", { yPercent: 100 }, { yPercent: 0 }, "same3")
+    .addLabel("same1")
+    .addLabel("same2")
+    .addLabel("same3");
 
 ScrollTrigger.create({
-  animation:t2,
-  trigger:".ring_box",
-  scrub:true,
-  pin:true
-})
+    animation: t2,
+    trigger: ".ring_box",
+    scrub: true,
+    pin: true
+});
+
+gsap.registerPlugin(MotionPathPlugin);
+
+const t3 = gsap.to(".circle", {
+    duration: 5,
+    ease: "power1.inOut",
+    motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0, 1]
+    }
+});
 
 
-  
+ScrollTrigger.create({
+    animation: t3,
+    trigger: "#stairs",
+    scrub: 4,
+    pin: true,
+    markers: true
+});
