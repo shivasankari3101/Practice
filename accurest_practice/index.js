729a349f71714de8a5df096300599be1b6bb0f1b
shivasankari3101 <shivasankari3101@gmail.com>
2021-07-15 16:59:25 +0530

    Half done

--- accurest_practice/index.js
+++ accurest_practice/index.js
@@ -1,16 +1,23 @@
-var tween1 = gsap.fromTo(".home", {
-    WebkitMaskPosition: "90%",
-    WebkitMaskSize: "50%",
-    duration: 5
-}, {
-    WebkitMaskPosition: "70%",
-    WebkitMaskSize: "2000%",
-    duration: 10
-});
+let t1 = gsap.timeline();
+t1.to(".home_text", { x: "-100%" })
+    .fromTo(".home_section1", {
+        WebkitMaskPosition: "90%",
+        WebkitMaskSize: "50%",
+        duration: 5
+    }, {
+        WebkitMaskPosition: "70%",
+        WebkitMaskSize: "2000%",
+        duration: 10
+    })
+    .fromTo(".services", {
+        x: "-100%"
+    }, {
+        x: "0%"
+    });
 
 ScrollTrigger.create({
-    animation: tween1,
-    trigger: ".home",
+    animation: t1,
+    trigger: ".home_section1",
     scrub: true,
     pin: true
 })
