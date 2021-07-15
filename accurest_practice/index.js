c52d3eb9e88bbc56a33942586b638cb716c4e0f5
shivasankari3101 <shivasankari3101@gmail.com>
2021-07-15 18:01:09 +0530

    Mask animation almost done

--- accurest_practice/index.js
+++ accurest_practice/index.js
@@ -1,12 +1,12 @@
 let t1 = gsap.timeline();
-t1.to(".home_text", { x: "-100%" })
+t1.to(".home_text", { x: "-100%", opacity: 0 })
     .fromTo(".home_section1", {
-        WebkitMaskPosition: "90%",
+        WebkitMaskPosition: "90% 20vh",
         WebkitMaskSize: "50%",
         duration: 5
     }, {
-        WebkitMaskPosition: "70%",
-        WebkitMaskSize: "2000%",
+        WebkitMaskPosition: "50.4805% -200vh",
+        WebkitMaskSize: "1000%",
         duration: 10
     })
     .fromTo(".services", {
