7027513cc8329de028c0075e32046fb4c2f4ee62
shivasankari3101 <shivasankari3101@gmail.com>
2021-08-18 18:47:29 +0530

    Omnyk working carousel

--- Omnyk/js/custom.js
+++ Omnyk/js/custom.js
@@ -7,3 +7,20 @@ $('.omnyk_stories_carousel').owlCarousel({
     autoplaySpeed: 3000,
     autoplayHoverPause: true
 })
+
+$('.omnyk-working-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 2
+        },
+        768: {
+            items: 3,
+            margin: 30
+        }
+    }
+})
