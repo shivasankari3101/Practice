2713f7ab96d51772191a63454f21d99ba3c5588c
shivasankari3101 <shivasankari3101@gmail.com>
2021-08-23 12:58:59 +0530

    Image sequencing

---
+++ dist/script.js
@@ -0,0 +1,46 @@
+const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+
+
+context.fillStyle = "red";
+context.fill();
+const frameCount = 115;
+const currentFrame = index => (
+    `C:/Users/Paramesh/Downloads/Gsap_example/apple-airpods-pro-animation-final-demo/dist/images/Smart Ring/Ring on Finger/SmartRingIntoFinger.${index}.png`
+)
+
+const preloadImages = () => {
+    for (let i = 1; i < frameCount; i++) {
+        const img = new Image();
+        img.src = currentFrame(i);
+        console.log(currentFrame(i));
+    }
+};
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width = 2500;
+canvas.height = 2500;
+img.onload = function() {
+    context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(
+        frameCount - 1,
+        Math.ceil(scrollFraction * frameCount)
+    );
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+preloadImages()
