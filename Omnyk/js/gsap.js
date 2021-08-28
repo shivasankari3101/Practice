fd96228ba2a5a6528c79425359ec21370a6a125c
shivasankari3101 <shivasankari3101@gmail.com>
2021-08-28 14:19:13 +0530

    Few more modification

--- Omnyk/js/gsap.js
+++ Omnyk/js/gsap.js
@@ -47,33 +47,29 @@ for (let i = 0; i < frameCount1; i++) {
     images1.push(img);
 }
 
-// Ring on finger animation
-function second_animation() {
-    gsap.timeline({
-            scrollTrigger: {
-                trigger: ".section2",
-                pin: ".section2",
-                scrub: true,
-                start: "top top"
 
-            },
-            ease: "circ.out"
-        })
-        .to(finger_imgs, {
-            frame: frameCount - 1,
-            snap: "frame",
-            onUpdate: render
-        }, "same1")
-        // .fromTo('.translate_text p', {
-        //     yPercent: 100,
-        // }, {
-        //     yPercent: -150,
-        //     stagger: {
-        //         each: 3
-        //     },
-        //     duration: 2
-        // }, "same1")
-        // .addLabel("same1");
+//Canvas for ring on charger animation
+
+const canvas2 = document.getElementById("ring_on_charger");
+const context2 = canvas2.getContext("2d");
+
+canvas2.width = 500;
+canvas2.height = 500;
+
+const frameCount2 = 60;
+const currentFrame2 = index => (
+    `./images/Smart Ring/Ring on Charger/SmartRingChargerKSPFile.${index+1}.png`
+);
+
+const images2 = []
+const finger_imgs2 = {
+    frame: 0
+};
+
+for (let i = 0; i < frameCount2; i++) {
+    const img = new Image();
+    img.src = currentFrame2(i);
+    images2.push(img);
 }
 
 //Header  animation
@@ -96,7 +92,28 @@ gsap.timeline({
     .fromTo(".section1 .text_content", { opacity: 1, yPercent: 0 }, { opacity: 0, yPercent: -40 }, "same")
     .to(".header_animation", { opacity: 0 })
     .fromTo(".section1 img", { opacity: 0, scale: 0.5 }, { opacity: 1, scale: 0.8 })
-    .to("header", { yPercent: -100 })
+    .to("header", { yPercent: -100 });
+
+
+// Ring on finger animation
+function second_animation() {
+    var t2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".section2",
+            pin: ".section2",
+            scrub: true,
+            start: "top top"
+
+        },
+        ease: "circ.out"
+    });
+    t2.to(finger_imgs, {
+        frame: frameCount - 1,
+        snap: "frame",
+        onUpdate: render
+    }, "same1")
+
+}
 
 
 // Functions on scroll trigger update
@@ -114,44 +131,51 @@ function render1() {
     context1.drawImage(images1[finger_imgs1.frame], 0, 0, canvas1.width, canvas1.height);
 }
 
+images2[0].onload = render2;
+
+function render2() {
+    context2.clearRect(0, 0, canvas1.width, canvas1.height);
+    context2.drawImage(images2[finger_imgs2.frame], 0, 0, canvas2.width, canvas2.height);
+}
+
+
+function gallery_animation() {
+    //Image gallery animation
+    const images3 = gsap.utils.toArray(".image");
+
+    images3.forEach((image, i) => {
+        gsap.from(image, {
+            scrollTrigger: {
+                trigger: image,
+                scrub: 3,
+                start: "top center",
+                end: "top center",
+            },
+            opacity: 0,
+            scale: 0.95,
+            yPercent: -20,
+            ease: "circ.out"
+        })
+    })
+}
 
 
 
-
-
-
-
-//Image gallery animation
-const images3 = gsap.utils.toArray(".image");
-
-images3.forEach((image, i) => {
-    gsap.from(image, {
+function omnyk_stories() {
+    //Fade in animation of Omnyk stories
+    gsap.from(".omnyk_stories", {
         scrollTrigger: {
-            trigger: image,
+            trigger: ".omnyk_stories",
             scrub: 3,
             start: "top center",
             end: "top center",
+            ease: "circ.out"
         },
-        opacity: 0,
-        scale: 0.95,
-        yPercent: -20,
-        ease: "circ.out"
+        opacity: 0
     })
-})
+}
 
-
-
-//Fade in animation of Omnyk stories
-gsap.from(".omnyk_stories", {
-    scrollTrigger: {
-        trigger: ".omnyk_stories",
-        scrub: 3,
-        start: "top center",
-        end: "top center",
-        ease: "circ.out"
-    },
-    opacity: 0
-})
+omnyk_stories();
 
 
 //Translate text animation for flickering text
@@ -162,7 +186,7 @@ const translate_texts = gsap.utils.toArray(".section4_translate_text h1");
 var t1 = gsap.timeline({
     scrollTrigger: {
         pin: ".section4",
-        scrub: 2,
+        scrub: 3,
         ease: "SlowMo"
     }
 });
@@ -181,3 +205,26 @@ translate_texts.forEach((translate_text, i) => {
         })
     }
 })
+
+
+// Ring on charger function
+gsap.timeline({
+        scrollTrigger: {
+            trigger: ".section6",
+            pin: ".section6",
+            scrub: 1,
+            start: "top top"
+        },
+        ease: "circ.out",
+        endTrigger: gallery_animation
+    })
+    .to(finger_imgs2, {
+        frame: frameCount2 - 1,
+        snap: "frame",
+        onUpdate: render2
+    })
+
+
+
+
+gallery_animation();
